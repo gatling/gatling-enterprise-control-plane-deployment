f555e7f5e0cbccc14fa4efcaae6cbaa43fe339ab
katwaGatling <katwa@gatling.io>
2024-05-15 19:18:38 +0300

    remove main, inputs and outputs

--- gatling-terraform/aws/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ControlPlaneUrl" {
-  value       = module.control-plane.alb_dns_name != "" ? "http://${module.control-plane.alb_dns_name}" : null
-  description = "Your Control Plane URL:"
-}
