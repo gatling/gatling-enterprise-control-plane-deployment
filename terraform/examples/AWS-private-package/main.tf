39c6a3ee1adb1bbfbcba2bc6bd6dcaac8a8a5924
katwaGatling <katwa@gatling.io>
2024-05-17 15:47:53 +0300

    remove whitespaces

--- terraform/examples/AWS-private-package/main.tf
+++ terraform/examples/AWS-private-package/main.tf
@@ -13,7 +13,7 @@ module "location" {
   region             = "eu-west-1"
   subnet_ids         = ["subnet-a", "subnet-b"]
   security_group_ids = ["sg-id"]
-  instance_type      = "c6i.xlarge" 
+  instance_type      = "c6i.xlarge"
 }
 
 module "control-plane" {
@@ -26,6 +26,6 @@ module "control-plane" {
   conf_s3_name        = "conf_s3_name"
   conf_s3_object_name = "control-plane.conf"
   locations           = [module.location]
-  alb_security_group_ids = ["sg-id"] 
+  alb_security_group_ids = ["sg-id"]
   private_package        = module.private-package
 }
