6347fba93a0b1c3a59d53903fa0b94efbdfe30c5
katwaGatling <katwa@gatling.io>
2024-05-27 14:40:06 +0200

    comment instance_type on aws examples

--- terraform/examples/AWS-private-package/main.tf
+++ terraform/examples/AWS-private-package/main.tf
@@ -13,19 +13,19 @@ module "location" {
   region             = "eu-west-1"
   subnet_ids         = ["subnet-a", "subnet-b"]
   security_group_ids = ["sg-id"]
-  instance_type      = "c6i.xlarge"
+  //instance_type      = "c6i.xlarge"
 }
 
 module "control-plane" {
-  source              = "git::git@github.com:gatling/gatling-enterprise-control-plane-deployment//terraform/aws/control-plane"
-  name                = "name"
-  token               = "token"
-  vpc                 = "vpc-id"
-  subnet_ids          = ["subnet-a", "subnet-b"]
-  security_group_ids  = ["sg-id"]
-  conf_s3_name        = "conf_s3_name"
-  conf_s3_object_name = "control-plane.conf"
-  locations           = [module.location]
+  source                 = "git::git@github.com:gatling/gatling-enterprise-control-plane-deployment//terraform/aws/control-plane"
+  name                   = "name"
+  token                  = "token"
+  vpc                    = "vpc-id"
+  subnet_ids             = ["subnet-a", "subnet-b"]
+  security_group_ids     = ["sg-id"]
+  conf_s3_name           = "conf_s3_name"
+  conf_s3_object_name    = "control-plane.conf"
+  locations              = [module.location]
   alb_security_group_ids = ["sg-id"]
   private_package        = module.private-package
 }
