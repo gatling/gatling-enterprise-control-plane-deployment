f8f552176ec2b368650c8f3c09fa9e6a6cad04a8
karimatwa <karim-atwa@hotmail.com>
2024-09-10 12:02:09 +0200

    feat: azure private locations & packages

---
+++ terraform/examples/AZURE-private-package/main.tf
@@ -0,0 +1,31 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "subscription-id"
+}
+
+module "private-package" {
+  source               = "git::git@github.com:gatling/gatling-enterprise-control-plane-deployment//terraform/azure/private-package"
+  container_name       = "gatling-cp"
+  storage_account_name = "storage-account-name"
+}
+
+module "location" {
+  source              = "git::git@github.com:gatling/gatling-enterprise-control-plane-deployment//terraform/azure/location"
+  id                  = "prl_azure"
+  region              = "westeurope"
+  resource_group_name = "resource-group-name"
+  virtual_network     = "vpc-id"
+  subnet_name         = "default"
+  //size                = "Standard_A4_v2"
+}
+
+module "control-plane" {
+  source               = "git::git@github.com:gatling/gatling-enterprise-control-plane-deployment//terraform/azure/control-plane"
+  container_name       = "gatling-cp"
+  token                = "token"
+  region               = "westeurope"
+  resource_group_name  = "resource-group-name"
+  storage_account_name = "storage-account-name"
+  locations            = [module.location]
+  private_package      = module.private-package
+}
