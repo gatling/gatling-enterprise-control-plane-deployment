db77e17a0dba93da6da27a787dd8caecacbc7098
karimatwa <karim-atwa@hotmail.com>
2024-09-11 02:58:32 +0200

    fix: aws terraform config conditional type checking

--- terraform/aws/control-plane/modules/s3/main.tf
+++ terraform/aws/control-plane/modules/s3/main.tf
@@ -2,16 +2,29 @@ data "aws_s3_bucket" "s3_conf" {
   bucket = var.name
 }
 
+locals {
+  base_content = {
+    token        = var.token
+    description  = var.description
+    locations    = [for location in var.locations : location.conf]
+  }
+
+  full_content = {
+    control-plane = merge(
+      var.extra_content,
+      local.base_content,
+      length(var.private_package) > 0 ? {
+        repository = var.private_package.conf
+      } : {}
+    )
+  }
+
+  json_content = jsonencode(local.full_content)
+}
+
 resource "aws_s3_object" "conf" {
   bucket       = data.aws_s3_bucket.s3_conf.id
   key          = var.object_name
   content_type = "application/json"
-  content = jsonencode({
-    control-plane : merge(var.extra_content, {
-      token : var.token,
-      description : var.description,
-      locations : [for location in var.locations : location.conf]
-      repository : length(var.private_package) > 0 ? var.private_package.conf : null
-    })
-  })
+  content      = local.json_content
 }
