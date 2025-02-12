d969f75b2b41d40e74180a872b6020431e18100f
Karim Atwa <karim-atwa@hotmail.com>
2025-02-12 11:24:42 +0100

    chore: update examples

--- terraform/examples/GCP-private-location/main.tf
+++ terraform/examples/GCP-private-location/main.tf
@@ -4,19 +4,25 @@ provider "google" {
 }
 
 module "location" {
-  source  = "git::git@github.com:gatling/gatling-enterprise-control-plane-deployment//terraform/gcp/location"
-  id      = "prl_gcp"
-  project = "project-id"
-  zone    = "zone-a"
+  source       = "git::git@github.com:gatling/gatling-enterprise-control-plane-deployment//terraform/gcp/location"
+  id           = "prl_gcp"
+  project      = "project-id"
+  zone         = "zone-a"
   //machine_type = "c3-highcpu-4"
   //engine       = "classic"
+  //enterprise_cloud = {
+    //url = "" // http://private-location-forward-proxy/gatling
+  //}
 }
 
 module "control-plane" {
-  source    = "git::git@github.com:gatling/gatling-enterprise-control-plane-deployment//terraform/gcp/control-plane"
-  name      = "name"
-  token     = "token"
-  zone      = "zone-a"
-  network   = "network"
-  locations = [module.location]
+  source            = "git::git@github.com:gatling/gatling-enterprise-control-plane-deployment//terraform/gcp/control-plane"
+  name              = "name"
+  token_secret_name = "token_secret_name"
+  zone              = "zone-a"
+  network           = "network"
+  locations         = [module.location]
+  //enterprise_cloud = {
+  //url = ""  // http://private-control-plane-forward-proxy/gatling
+  //}
 }
--- terraform/examples/GCP-private-package/main.tf
+++ terraform/examples/GCP-private-package/main.tf
@@ -16,14 +16,20 @@ module "location" {
   zone    = "zone-a"
   //machine_type = "c3-highcpu-4"
   //engine       = "classic"
+  //enterprise_cloud = {
+  //url = ""  // http://private-location-forward-proxy/gatling
+  //}
 }
 
 module "control-plane" {
-  source          = "git::git@github.com:gatling/gatling-enterprise-control-plane-deployment//terraform/gcp/control-plane"
-  name            = "name"
-  token           = "token"
-  zone            = "zone-a"
-  network         = "network"
-  locations       = [module.location]
-  private_package = module.private-package
+  source            = "git::git@github.com:gatling/gatling-enterprise-control-plane-deployment//terraform/gcp/control-plane"
+  name              = "name"
+  token_secret_name = "token_secret_name"
+  zone              = "zone-a"
+  network           = "network"
+  locations         = [module.location]
+  private_package   = module.private-package
+  //enterprise_cloud = {
+  //url = ""  // http://private-control-plane-forward-proxy/gatling
+  //}
 }
