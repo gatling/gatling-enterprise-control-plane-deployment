f616c8737abed7b6fd95fdf92fb7996633a2085e
Karim Atwa <karim-atwa@hotmail.com>
2025-03-17 09:48:35 +0100

    chore: add nested object for private packages args

--- terraform/azure/private-package/variables.tf
+++ terraform/azure/private-package/variables.tf
@@ -1,45 +1,61 @@
 variable "storage_account_name" {
   type        = string
   description = "Storage account name to be used with the control plane."
+
+  validation {
+    condition     = length(var.storage_account_name) > 0
+    error_message = "Storage account name must not be empty."
+  }
 }
 
 variable "container_name" {
   type        = string
   description = "Container name of the control plane."
+
+  validation {
+    condition     = length(var.container_name) > 0
+    error_message = "Container name must not be empty."
+  }
 }
 
 variable "path" {
   type        = string
-  description = "Storage path on the S3 private package."
+  description = "Storage path for private package."
   default     = ""
 }
 
-variable "uploadDir" {
-  type        = string
+variable "upload" {
   description = "Control Plane Repository Server Temporary Upload Directory."
-  default     = "/tmp"
+  type = object({
+    directory = string
+  })
+  default = {
+    directory = "/tmp"
+  }
 }
 
-variable "port" {
-  type        = number
-  description = "Control Plane Repository Server Port."
-  default     = 8080
-}
+variable "server" {
+  description = "Control Plane Repository Server configuration."
+  type = object({
+    port        = number
+    bindAddress = string
+    certificate = optional(object({
+      path     = string
+      password = optional(string)
+    }))
+  })
+  default = {
+    port        = 8080,
+    bindAddress = "0.0.0.0",
+    certificate = null
+  }
 
-variable "bindAddress" {
-  type        = string
-  description = "Control Plane Repository Server Bind Address."
-  default     = "0.0.0.0"
-}
-
-variable "certPath" {
-  type        = string
-  description = "Control Plane Repository Server Certificate Path."
-  default     = ""
-}
-
-variable "certPassword" {
-  type        = string
-  description = "Control Plane Repository Server Certificate Password."
-  default     = ""
+  validation {
+    condition     = var.server.port > 0 && var.server.port <= 65535
+    error_message = "Server port must be between 1 and 65535."
+  }
+  validation {
+    condition     = length(var.server.bindAddress) > 0
+    error_message = "Server bindAddress must not be empty."
+  }
 }
