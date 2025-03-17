0d54b76fcc329ec49bbc90031d2d13b953b905a4
Karim Atwa <karim-atwa@hotmail.com>
2025-03-17 14:45:24 +0100

    chore: cleanup control-plane and add arg validations

--- terraform/azure/control-plane/variables.tf
+++ terraform/azure/control-plane/variables.tf
@@ -1,44 +1,99 @@
 variable "name" {
-  type        = string
   description = "Name of the control plane"
+  type        = string
+
+  validation {
+    condition     = length(var.name) > 0
+    error_message = "The name of the control plane must not be empty."
+  }
 }
 
 variable "description" {
-  type        = string
   description = "Description of the control plane."
+  type        = string
   default     = "My Azure control plane description"
 }
 
-variable "image" {
+variable "vault_name" {
+  description = "Vault name where the control plane token secret is stored."
   type        = string
-  description = "Image of the control plane."
-  default     = "gatlingcorp/control-plane:latest"
+
+  validation {
+    condition     = length(var.vault_name) > 0
+    error_message = "Vault name must not be empty."
+  }
+}
+
+variable "secret_id" {
+  description = "Secret identifier where the control token plane is stored."
+  type        = string
+
+  validation {
+    condition     = length(var.secret_id) > 0
+    error_message = "Secret identifier must not be empty."
+  }
 }
 
 variable "region" {
-  type        = string
   description = "Region of the location."
+  type        = string
+
+  validation {
+    condition     = length(var.region) > 0
+    error_message = "Region must not be empty."
+  }
 }
 
 variable "resource_group_name" {
-  type        = string
   description = "Resource group name."
+  type        = string
+
+  validation {
+    condition     = length(var.resource_group_name) > 0
+    error_message = "Resource group must not be empty."
+  }
+}
+
+variable "container" {
+  description = "Container settings."
+  type = object({
+    image   = string
+    command = optional(list(string))
+    env = optional(list(object({
+      name        = string
+      value       = optional(string)
+      secret_name = optional(string)
+    })))
+    cpu    = number
+    memory = string
+  })
+  default = {
+    image  = "gatlingcorp/control-plane:latest"
+    cpu    = 1.0
+    memory = "2Gi"
+    command = []
+    env     = []
+  }
 }
 
 variable "storage_account_name" {
-  type        = string
   description = "Storage account name to be used with the control plane."
-}
-
-variable "conf_share_file_name" {
   type        = string
-  description = "Name of the configuration object to be stored inside the storage share."
-  default     = "control-plane.conf"
+
+  validation {
+    condition     = length(var.storage_account_name) > 0
+    error_message = "Storage account name must not be empty."
+  }
 }
 
 variable "locations" {
   description = "JSON configuration for the locations."
-  type        = list(map(any))
+  type        = list(any)
+
+  validation {
+    condition     = length(var.locations) > 0
+    error_message = "At least one private location must be specified."
+  }
 }
 
 variable "private_package" {
@@ -47,40 +102,12 @@ variable "private_package" {
   default     = {}
 }
 
-variable "extra_content" {
-  type    = map(any)
-  default = {}
-}
-
-variable "command" {
-  description = "Control plane image command"
-  type        = list(string)
-  default     = []
-}
-
 variable "enterprise_cloud" {
   type    = map(any)
   default = {}
 }
 
-variable "vault_name" {
-  description = "Vault name where the control plane token secret is stored."
-  type        = string
-}
-
-variable "secret_id" {
-  description = "Secret identifier where the control token plane is stored."
-  type = string
-}
-
-variable "container_cpu" {
-  description = "Control Plane container CPU allocation."
-  type    = number
-  default = 1.0
-}
-
-variable "container_memory" {
-  description = "Control Plane container memory allocation."
-  type    = string
-  default = "2Gi"
+variable "extra_content" {
+  type    = map(any)
+  default = {}
 }
