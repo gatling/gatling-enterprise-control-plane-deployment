029427f6f45bb0271dc14733ce1ccc21ff677ec0
Karim Atwa <karim-atwa@hotmail.com>
2025-03-21 18:23:22 +0100

    chore: add common aws_region resource for controlplane & pass locations to iam

--- terraform/aws/control-plane/main.tf
+++ terraform/aws/control-plane/main.tf
@@ -1,7 +1,11 @@
+data "aws_region" "current" {}
+
 module "iam" {
   source           = "./modules/iam"
+  aws_region       = data.aws_region.current.name
   name             = var.name
   token-secret-arn = var.token-secret-arn
+  locations        = var.locations
   private-package  = var.private-package
   cloudwatch-logs  = var.task.cloudwatch-logs
   ecr              = var.task.ecr
@@ -9,6 +13,7 @@ module "iam" {
 
 module "ecs" {
   source           = "./modules/ecs"
+  aws_region       = data.aws_region.current.name
   name             = var.name
   description      = var.description
   subnets          = var.subnets
