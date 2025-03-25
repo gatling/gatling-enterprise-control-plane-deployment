19d163c0fd7c06f5e15823d51a7fd6147f1980ea
Karim Atwa <karim-atwa@hotmail.com>
2025-03-25 15:19:01 +0100

    chore: update examples

--- terraform/examples/AWS-private-package/main.tf
+++ terraform/examples/AWS-private-package/main.tf
@@ -1,36 +1,72 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "<Region>"
 }
 
+# Configure a private package (control plane repository & server) based on AWS S3
+# Reference: https://docs.gatling.io/reference/install/cloud/private-locations/private-packages/#gcp-cloud-storage
+# Reference: https://docs.gatling.io/reference/install/cloud/private-locations/private-packages/#control-plane-server
 module "private-package" {
-  source = "git::git@github.com:gatling/gatling-enterprise-control-plane-deployment//terraform/aws/private-package"
-  bucket = "bucket"
+  source = "git::https://github.com/gatling/gatling-enterprise-control-plane-deployment//terraform/aws/private-package"
+  bucket = "<S3BucketName>"
 }
 
+# Configure a AWS private location
+# Reference: https://docs.gatling.io/reference/install/cloud/private-locations/gcp/configuration/#control-plane-configuration-file
 module "location" {
-  source             = "git::git@github.com:gatling/gatling-enterprise-control-plane-deployment//terraform/aws/location"
-  id                 = "prl_aws"
-  region             = "eu-west-1"
-  subnet_ids         = ["subnet-a", "subnet-b"]
-  security_group_ids = ["sg-id"]
-  //instance_type      = "c7i.xlarge"
-  //engine             = "classic"
-  //enterprise_cloud = {
-    //url = "http://private-location-forward-proxy/gatling"
-  //}
+  source          = "git::https://github.com/gatling/gatling-enterprise-control-plane-deployment//terraform/aws/location"
+  id              = "prl_aws"
+  region          = "<Region>"
+  subnets         = ["<SubnetId>"]
+  security-groups = ["<SecurityGroupId>"]
+  # instance-type   = "c7i.xlarge"
+  # engine          = "classic"
+  # ami = {
+  #   type = "certified"
+  #   java = "latest"
+  #   id   = "ami-00000000000000000"
+  # }
+  # spot                       = false
+  # auto-associate-public-ipv4 = true
+  # elastic-ips                = ["203.0.113.3", "203.0.113.4"]
+  # profile-name               = "profile-name"
+  # iam-instance-profile = "iam-instance-profile"
+  # tags                       = {}
+  # tags-for = {
+  #   instance          = {}
+  #   volume            = {}
+  #   network-interface = {}
+  # }
+  # system-properties   = {}
+  # java-home           = "/usr/lib/jvm/zulu"
+  # jvm-options         = []
+  # enterprise-cloud = {
+  #   Setup the proxy configuration for the private location
+  #   Reference: https://docs.gatling.io/reference/install/cloud/private-locations/network/#configuring-a-proxy
+  # }
 }
 
+# Create a control plane based on AWS ECS
+# Reference: https://docs.gatling.io/reference/install/cloud/private-locations/gcp/installation/
 module "control-plane" {
-  source             = "git::git@github.com:gatling/gatling-enterprise-control-plane-deployment//terraform/aws/control-plane"
-  name               = "control-plane"
-  token_secret_arn   = "aws-secrets-manager-secret-arn"
-  subnet_ids         = ["subnet-a", "subnet-b"]
-  security_group_ids = ["sg-id"]
-  locations          = [module.location]
-  private_package    = module.private-package
-  //cloudWatch_logs    = true
-  //ecr                = false
-  //enterprise_cloud = {
-    //url = "http://private-control-plane-forward-proxy/gatling"
-  //}
+  source           = "git::https://github.com/gatling/gatling-enterprise-control-plane-deployment//terraform/aws/control-plane"
+  name             = "<Name>"
+  token-secret-arn = "<TokenSecretARN>"
+  subnets          = ["<SubnetId>"]
+  security-groups  = ["<SecurityGroupId>"]
+  locations        = [module.location]
+  private-package  = module.private-package
+  # task = {
+  #   image           = "gatlingcorp/control-plane:latest"
+  #   command         = []
+  #   secrets         = []
+  #   environment     = []
+  #   cpu             = "1024"
+  #   memory          = "3072"
+  #   cloudwatch-logs = true
+  #   ecr             = false
+  # }
+  # enterprise-cloud = {
+  #   Setup the proxy configuration for the control plane
+  #   Reference: https://docs.gatling.io/reference/install/cloud/private-locations/network/#configuring-a-proxy
+  # }
 }
