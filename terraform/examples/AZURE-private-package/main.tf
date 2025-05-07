6cc817179fa9561c2948e8f63844aabfe144751f
Karim Atwa <karim-atwa@hotmail.com>
2025-05-07 11:00:45 +0200

    chore: update private packages example and README

--- terraform/examples/AZURE-private-package/main.tf
+++ terraform/examples/AZURE-private-package/main.tf
@@ -54,22 +54,41 @@ module "location" {
 # Create a control plane based on Azure Container App
 # Reference: https://docs.gatling.io/reference/install/cloud/private-locations/azure/installation/
 module "control-plane" {
-  source               = "git::https://github.com/gatling/gatling-enterprise-control-plane-deployment//terraform/azure/control-plane"
-  name                 = "<Name>"
-  description          = "My Azure control plane description"
-  vault-name           = "<Vault>"
-  secret-id            = "<SecretIdentifier>"
-  region               = "<Region>"
-  resource-group-name  = "<ResourceGroup>"
-  storage-account-name = "<StorageAccount>"
-  locations            = [module.location]
-  private-package      = module.private-package
-  # container = {
+  source              = "git::https://github.com/gatling/gatling-enterprise-control-plane-deployment//terraform/azure/control-plane"
+  name                = "<Name>"
+  description         = "My Azure control plane description"
+  vault-name          = "<Vault>"
+  token-secret-id     = "<TokenSecretIdentifier>"
+  region              = "<Region>"
+  resource-group-name = "<ResourceGroup>"
+  locations           = [module.location]
+  private-package     = module.private-package
+  # container-app = {
+  #   init = {
+  #     image = "busybox"
+  #   }
   #   cpu         = 1.0
   #   memory      = "2Gi"
   #   image       = "gatlingcorp/control-plane:latest"
   #   command     = []
   #   environment = []
+  # }
+  # git = {
+  #   # Configure git credentials for the control plane. Requires builder image: "gatlingcorp/control-plane:latest-builder"
+  #   # Reference: https://docs.gatling.io/reference/execute/cloud/user/build-from-sources/
+  #   host = "github.com"
+  #   credentials = {
+  #     username        = "karimatwa"
+  #     token-secret-id = "https://gatlingvault.vault.azure.net/secrets/github-token/fd927b5f46c648fb84302991e78403d2"
+  #   }
+  #   ssh = {
+  #     storage-account-name = "<StorageAccountName>"
+  #     file-share-name      = "<FileShareName>"
+  #     file-name            = "<FileName>"
+  #   }
+  #   cache = {
+  #     paths = ["/app/.m2", "/app/.gradle", "/app/.sbt", "/app/.npm"]
+  #   }
   # }
   # enterprise-cloud = {
   #   Setup the proxy configuration for the private location
