4f6f3f63e1756470a11860da469a6b5266963523
Karim Atwa <karim-atwa@hotmail.com>
2025-05-07 10:26:06 +0200

    chore: cleanup storage-account module and use it to retrieve storage account

--- terraform/azure/control-plane/modules/storage-account/outputs.tf
+++ terraform/azure/control-plane/modules/storage-account/outputs.tf
@@ -1,9 +1,3 @@
-output "primary-access-key" {
-  value       = data.azurerm_storage_account.gatling_storage_account.primary_access_key
-  description = "Primary key for the storage account"
-}
-
-output "share-name" {
-  value       = azurerm_storage_share.gatling_storage_share.name
-  description = "Storage share name used to store the configuration."
+output "primary_access_key" {
+  value = data.azurerm_storage_account.gatling_storage_account.primary_access_key
 }
