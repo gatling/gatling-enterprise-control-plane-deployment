f387fcea3fef0123ba0ab6a639195e5c82cf7f83
Alhussein Ibrahim <husseinsamy32@gmail.com>
2025-09-16 16:47:36 +0200

    chore: adjust aws private packages task parameter in control plane

--- terraform/examples/AWS-private-package/main.tf
+++ terraform/examples/AWS-private-package/main.tf
@@ -69,12 +69,14 @@ module "control-plane" {
   security-groups  = ["<SecurityGroupId>"]
   locations        = [module.location]
   private-package  = module.private-package
-  # extra-init-command     = "mkdir /app/conf/.aws && echo -e \"[profile_name]\naws_access_key_id = $AWS_ACCESS_KEY_ID\naws_secret_access_key = $AWS_SECRET_KEY\n\" > /app/conf/.aws/credentials"
   # task = {
   #   cpu             = "1024"
   #   memory          = "3072"
   #   init = {
   #     image = "busybox"
+  #     command = []
+  #     secrets = [],
+  #     environment = []
   #   }
   #   image           = "gatlingcorp/control-plane:latest"
   #   command         = []
